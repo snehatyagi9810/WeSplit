02b53b22f408605860591d020c544011805b9e28
snehatyagi9810 <snehtyagi9810@gmail.com>
2025-12-31 15:39:19 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sneha Tyagi on 31/12/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
